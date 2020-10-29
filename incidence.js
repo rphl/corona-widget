cc81c404456285ae35959652fd053117f2120c5e
Glow <23194902+Nils89@users.noreply.github.com>
2020-10-29 16:15:26 +0100

    Öffnet das RKI Dashboard beim Klick auf das Widget

--- incidence.js
+++ incidence.js
@@ -90,6 +90,7 @@ async function createWidget() {
         data[areaName] = _data
     }
     const list = new ListWidget()
+    list.url = "https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4"
     const headerLabel = list.addStack()
     headerLabel.useDefaultPadding()
     headerLabel.centerAlignContent()
