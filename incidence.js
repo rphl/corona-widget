76e0e060bf883d13b26d095e7344971df466c0c6
Andreas <wilhelm.andreas@gmail.com>
2020-11-16 07:17:02 +0100

    Update incidence.js

--- incidence.js
+++ incidence.js
@@ -21,8 +21,8 @@ const CONFIG_MAX_CACHED_DAYS = 14 // WARNING!!! Smaller values will delete saved
 const CONFIG_CSV_RVALUE_FIELDS = ['Schätzer_7_Tage_R_Wert', 'Punktschätzer des 7-Tage-R Wertes'] // try to find possible field (column) with rvalue, because rki is changing columnsnames and encoding randomly on each update
 const CONFIG_REFRESH_INTERVAL = 60 * 60 // interval the widget is update in (in seconds)
 const CONFIG_SHOW_CASES_TREND_ARROW = true // show trend arrow for cases
-const CONFIG_SHOW_DECIMALS = true // show decimals for area incidence value
-const CONFIG_SHOW_DECIMALS_BL = true // show decimals for state incidence value
+const CONFIG_SHOW_DECIMALS = false // show decimals for area incidence value
+const CONFIG_SHOW_DECIMALS_BL = false // show decimals for state incidence value
 
 
 // ============= ============= ============= ============= =================
